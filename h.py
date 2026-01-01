15ed1895b72705af453cc0ab5584f6800012f6e4
Hasan-Mahmood231 <hk4145559@gmail.com>
2026-01-01 09:41:58 +0800

    delete extra files clean the repo.

--- h.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Hellow guys")
-st.subheader("Select your fav language.")
-lang = st.selectbox("My fav language is ",['cpp','python','java','R','skala'])
-st.write(f"Your fav language is {lang}")
-st.success("your language is really great")
